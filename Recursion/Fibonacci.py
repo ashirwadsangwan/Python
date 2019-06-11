267bdae52bea47b83f009992b4ea2dcc794469c7
Ashirwad Sangwan <ashirwadsangwan@gmail.com>
2019-06-12 02:43:03 +0530

    Fibonacci

---
+++ Recursion/Fibonacci.py
@@ -0,0 +1,23 @@
+def fib(n): return n if n<=1 else fib(n-1)+fib(n-2)
+
+## Iterative Program
+
+def fibonacci(n):
+
+    # Taking 1st two fibonacci nubers as 0 and 1
+    FibArray = [0, 1]
+
+    while len(FibArray) < n + 1:
+        FibArray.append(0)
+
+    if n <= 1:
+        return n
+    else:
+        if FibArray[n - 1] == 0:
+            FibArray[n - 1] = fibonacci(n - 1)
+
+        if FibArray[n - 2] == 0:
+            FibArray[n - 2] = fibonacci(n - 2)
+
+    FibArray[n] = FibArray[n - 2] + FibArray[n - 1]
+    return FibArray[n]
