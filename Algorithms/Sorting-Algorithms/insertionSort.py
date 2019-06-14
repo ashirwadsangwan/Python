0ed7bc334fc6084c2cad8a3a9355dfe62d87ca17
Ashirwad Sangwan <ashirwadsangwan@gmail.com>
2019-06-15 02:35:07 +0530

    Insertion Sort Implemented!

---
+++ Algorithms/Sorting-Algorithms/insertionSort.py
@@ -0,0 +1,23 @@
+
+def insertionSort(A):
+    n = len(A)
+
+    for i in range(1, n):
+        value = A[i]
+        hole = i
+
+        while (hole>0 and A[hole-1]>value):
+            A[hole] = A[hole-1]
+            hole = hole-1
+        A[hole] = value
+    return A
+
+
+
+
+
+
+if __name__ == "__main__":
+    A = [7,10,3,1,5,2,7,0,1]
+    sorted_array = insertionSort(A)
+    print(sorted_array)
