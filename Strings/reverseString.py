a76083bc2bd07e12648473dfc490d231e5d319a8
Ashirwad Sangwan <43483454+ashirwadsangwan@users.noreply.github.com>
2019-06-26 07:39:18 +0530

    Update reverseString.py

--- Strings/reverseString.py
+++ Strings/reverseString.py
@@ -9,8 +9,8 @@ def reverse_str(string):
   
   ## Alternative
   
-  def reverse(string):
-    if len(string)==0:
-        return string
-    else:
-        return reverse(string[1:]) + string[0]
+def reverse(string):
+  if len(string)==0:
+      return string
+  else:
+      return reverse(string[1:]) + string[0]
