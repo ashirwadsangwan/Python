080ef63a7ad4ebdbd47579c4115a305af643b110
Ashirwad Sangwan <ashirwadsangwan@gmail.com>
2019-07-07 11:19:41 +0530

    Added test cases in Peak finding problem #2

--- MIT-6.006/01-PeakFinding/peakFinding.py
+++ MIT-6.006/01-PeakFinding/peakFinding.py
@@ -1,6 +1,14 @@
 def peak(array):
     peaks = []
-    
+
+    if len(array)==0:
+        return "The array has zero elements"
+    elif len(array)==1:
+        return [0]
+    elif len(array)==2:
+        peaks = [0,1]
+        return peaks
+
     if array[0]>array[1]:
         peaks.append(0)
 
@@ -13,5 +21,9 @@ def peak(array):
 
     return peaks
 
-arr = [1,0,3,2,1,4,3,5,2,6]
-print(peak(arr))
+
+if __name__ == "__main__":
+    print(peak([1])) # when array has only one elements
+    print(peak([1, 2])) # when array has two elements
+    print(peak([1, 0, 3]))
+    print(peak([1,0,2,1,3,2,5,2,6]))
